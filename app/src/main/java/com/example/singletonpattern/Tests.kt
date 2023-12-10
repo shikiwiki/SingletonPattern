7331985c53564b68c359a74e21633ad508d688a5
ViktoriaAstashonok <122493150+ViktoriaAstashonok@users.noreply.github.com>
2023-12-10 08:31:50 +0300

    main added

---
+++ app/src/main/java/com/example/singletonpattern/Tests.kt
@@ -0,0 +1,9 @@
+package com.example.singletonpattern
+
+fun main() {
+    val s1 = Singleton1.getInstance()
+    val s2 = Singleton2.getInstance()
+    val s3 = Singleton3.getInstance()
+    val s4 = Singleton4.getInstance()
+    val s5 = Singleton5.getInstance()
+}
