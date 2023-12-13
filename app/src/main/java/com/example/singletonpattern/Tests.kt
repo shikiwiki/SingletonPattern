ec002ba1eef2aec448cde85bf59959f30cca8f1a
ViktoriaAstashonok <122493150+ViktoriaAstashonok@users.noreply.github.com>
2023-12-13 09:03:49 +0300

    true singleton added

--- app/src/main/java/com/example/singletonpattern/Tests.kt
+++ app/src/main/java/com/example/singletonpattern/Tests.kt
@@ -1,6 +1,7 @@
 package com.example.singletonpattern
 
 fun main() {
+    val trueSingleton = TrueSingleton
     val s1 = Singleton1.getInstance()
     val s2 = Singleton2.getInstance()
     val s3 = Singleton3.getInstance()
---
+++ app/src/main/java/com/example/singletonpattern/TrueSingleton.kt
@@ -0,0 +1,4 @@
+package com.example.singletonpattern
+
+object TrueSingleton {
+}
